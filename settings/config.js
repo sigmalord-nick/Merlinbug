3b19e6dc8e3d8dd0023b81790f7fd66d0e26c8e7
☠︎༆SIGMALORD༆☠︎👿👿 <sigmalordnicks@gmail.com>
2025-05-29 02:43:40 +0300

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -1,1 +1,9 @@
-s
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['6285754675168']
+global.gambar = "https://files.catbox.moe/h30cu2.png"
+
+// CREATE BY VINNTHEXDC DEVELOPER
+// CONTACT DEVELOPED :
+// Whatsapp : wa.me/6285810820809
+// Telegram : t.me/VinnRealll
+// SELAIN NO DI ATAS SUDAH DI PASTIKAN CLONE !!!
